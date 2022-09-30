991d37d9b0c5edd033e1bed730fca188dddb2911
Liam S. Crouch <liam@phoenixlan.no>
2022-09-30 16:26:03 +0200

    Add weak in practice RE task

--- RE/Weak In Practice/weak.c
+++ RE/Weak In Practice/weak.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void generate_lookup_table(int key, char* lookup, int size) {
+	//Pass 1
+	for(int i = 0; i < size; i++) {
+		lookup[i] = (char)((key >> (i%24)) ^ i ^ 0xba);
+	}
+	//Pass 2
+	for(int i = 0; i < size; i++) {
+		lookup[i] = lookup[i] ^ lookup[(i*3) % size];
+	}
+	// Pass 3
+	for(int i = 0; i < size; i++) {
+		lookup[i] = lookup[(i+1) % size] ^ lookup[i] ^ (char)0x13;
+	}
+}
+
+char* load(char* filename, int* file_size) {
+	FILE* in = fopen(filename, "rb");
+
+	fseek(in, 0L, SEEK_END);
+	*file_size = ftell(in);
+	fseek(in, 0L, SEEK_SET);
+
+	char* data = malloc(*file_size);
+	fread(data, 1, *file_size, in);
+	fclose(in);
+
+	return data;
+}
+
+void save(char* filename, char* data, int file_size) {
+	FILE* out = fopen(filename, "wb");
+	fwrite(data, 1, file_size, out);
+	fclose(out);
+}
+
 int main(int argc, char** argv) {
+	printf("Bjarnes SuperEncryptor 2000\n!!!extreme edition!!!\n");
 	if(argc != 4) {
-		printf("Invalid number of arguments.\nUsage: uberdecryptor [key] [file] [out]\n(The tool encrypts symmetrically)\n");
+		printf("\nERROR:\nInvalid number of arguments.\nUsage: superencryptor [key] [file] [out]\n(The tool encrypts symmetrically)\n");
 		return 1;
 	}
 
 	int num = atoi(argv[1]);
 
 	unsigned char lookup[0x1000];
-
-	//Pass 1
-	for(int i = 0; i < sizeof(lookup); i++) {
-		lookup[i] = num ^ ((unsigned char)i&0xFF) ^ 0xba;
-	}
-	//Pass 2
-	for(int i = 0; i < sizeof(lookup); i++) {
-		lookup[i] = lookup[i] ^ lookup[(i*3)%sizeof(lookup)];
-	}
-	// Pass 3
-	for(int i = 0; i < sizeof(lookup); i++) {
-		lookup[i] = lookup[(i+1)%sizeof(lookup)] ^ lookup[i] ^ 0x13;
-	}
-
-	FILE* in = fopen(argv[2], "rb");
-
-	fseek(in, 0L, SEEK_END);
-	int file_size = ftell(in);
-	fseek(in, 0L, SEEK_SET);
-
-	char* plaintext = malloc(file_size);
-	fread(plaintext, 1, file_size, in);
-	fclose(in);
+	generate_lookup_table(num, lookup, 0x1000);
+	
+	int file_size = 0;
+	char* plaintext = load(argv[2], &file_size);
 
 	// Encrypt
 	for(int i = 0; i < file_size; i++) {
-		plaintext[i] = plaintext[i] ^ lookup[i % sizeof(lookup)];
+		plaintext[i] = plaintext[i] ^ lookup[i % 0x1000];
 	}
 
 	// Save
-	FILE* out = fopen(argv[3], "wb");
-	fwrite(plaintext, 1, file_size, out);
-	fclose(out);
+	save(argv[3], plaintext, file_size);
 	printf("*ding*\n");
 
 	return 0;
